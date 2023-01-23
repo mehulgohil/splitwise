6bad375dadfc075b792eabd3921cdd1415f356dc
Mehul <mehulgohil75@gmail.com>
2023-01-23 22:31:45 +0530

    added output field

--- frontend/splitwise-frontend/src/Components/GetOweBy.js
+++ frontend/splitwise-frontend/src/Components/GetOweBy.js
@@ -3,6 +3,7 @@ import {useState} from "react";
 
 function GetOweBy() {
   const [mobileNo, setMobileNo] = useState();
+  const [output, setOutput] = useState("")
 
   function onMobileChange(e) {
     setMobileNo(e.target.value)
@@ -14,13 +15,14 @@ function GetOweBy() {
     };
     const response = await fetch('http://localhost:8080/transactions/oweBy/'+mobileNo, requestOptions);
     const data = await response.json();
-    alert(JSON.stringify(data))
+    setOutput(JSON.stringify(data))
   }
 
   return (
-    <FormControl style={{marginLeft: "20px"}}>
+    <FormControl style={{marginLeft: "20px", width: "50%"}}>
       <TextField id="standard-basic" label="Mobile No" variant="standard" onChange={onMobileChange}/>
       <Button variant="contained" style={{marginTop: "10px"}} onClick={getOweByDetails} >Submit</Button>
+      <TextField id="standard-basic" label="output" variant="standard" value={output} multiline/>
     </FormControl>
   )
 }
--- frontend/splitwise-frontend/src/Components/GetOweTo.js
+++ frontend/splitwise-frontend/src/Components/GetOweTo.js
@@ -3,6 +3,7 @@ import {Button, FormControl, TextField} from "@mui/material";
 
 function GetOweTo() {
   const [mobileNo, setMobileNo] = useState();
+  const [output, setOutput] = useState("")
 
   function onMobileChange(e) {
     setMobileNo(e.target.value)
@@ -14,13 +15,14 @@ function GetOweTo() {
     };
     const response = await fetch('http://localhost:8080/transactions/oweTo/'+mobileNo, requestOptions);
     const data = await response.json();
-    alert(JSON.stringify(data))
+    setOutput(JSON.stringify(data))
   }
 
   return (
-    <FormControl style={{marginLeft: "20px"}}>
+    <FormControl style={{marginLeft: "20px", width: "50%"}}>
       <TextField id="standard-basic" label="Mobile No" variant="standard" onChange={onMobileChange}/>
       <Button variant="contained" style={{marginTop: "10px"}} onClick={getOweByDetails}>Submit</Button>
+      <TextField id="standard-basic" label="output" variant="standard" value={output} multiline/>
     </FormControl>
   )
 }
